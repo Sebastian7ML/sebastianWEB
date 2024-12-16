5198277a4428d294ef164e7c281f9c1b5ad7d041
Sebastian7ML <sebastian7ml@gmail.com>
2024-12-16 20:52:58 +0100

    add AvionicsPics component

--- SebastianMejia/src/App.tsx
+++ SebastianMejia/src/App.tsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
 import logo from './assets/sm.svg'
 import './App.css'
+import AvionicsPics from './components/avionicsPics';
+
+const images = [
+  'https://images.desenio.com/zoom/wb0125-8batman-portrait50x70-55544-10774.jpg', // Replace these URLs with actual image URLs
+  'https://i.pinimg.com/736x/6a/11/09/6a11097af8e1e205ee946c3b2d62e125.jpg',
+  'https://static.eldiario.es/clip/f073a34b-1712-4ed0-a9d5-706fd12f3061_16-9-aspect-ratio_default_0.jpg'
+];
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        
         <a href="https://github.com/Sebastian7ML" target="_blank">
           <img src={logo} className="logo" alt="Sebas logo" />
         </a>
@@ -16,13 +21,12 @@ function App() {
       <h1>Sebastian Mejia</h1>
       <h2>Software Engineer & Avionics Technician</h2>
       <h3>// in short: Tech Enthusiast</h3>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <AvionicsPics images={images} />
     </>
   )
+
 }
 
 export default App
+
+
---
+++ SebastianMejia/src/components/AvionicsPics.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './avionicsPics.css'; 
+
+interface AvionicsPicsProps {
+    images: string[];
+}
+
+const AvionicsPics: React.FC<AvionicsPicsProps> = ({ images }) => {
+    return (
+        <div className="picture-row">
+            {images.map((image, index) => (
+                <img key={index} src={image} alt={`picture-${index}`} className="picture" />
+            ))}
+        </div>
+    );
+};
+
+export default AvionicsPics;
