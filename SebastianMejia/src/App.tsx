4d9a9b00125da8a6887e18a23b86bbe51f57d221
Sebastian7ML <sebastian7ml@gmail.com>
2025-02-09 18:25:00 +0100

    added nav component

--- SebastianMejia/src/App.tsx
+++ SebastianMejia/src/App.tsx
@@ -2,8 +2,9 @@ import logo from './assets/sm.svg'
 import './App.css'
 import AvionicsPics from './components/AvionicsPics';
 import ImageRow from './components/ImageRow';
+import Nav from './components/Nav';
 
-const images = [
+const avionicsImages = [
   'https://i.postimg.cc/wTDPqKxn/bo.jpg', 
   'https://i.postimg.cc/bY7WfYPQ/ec.jpg',
   'https://i.postimg.cc/7PGdRGFH/bell.jpg'
@@ -22,7 +23,12 @@ function App() {
       <h1>Sebastian Mejia</h1>
       <h2>Software Engineer & Avionics Technician</h2>
       <h3>// in short: Tech Enthusiast</h3>
-      <AvionicsPics images={images} />
+      <AvionicsPics images={avionicsImages} />
+      
+      <h4 id='avionics'>Avionics Technician</h4>
+      <p className='avionicsLife'>I worked for about 8 years at <a href="https://www.avionicacolombia.com.co" className='gulf-link' target='blank'>Gulf Coast Avionics Colombia</a>, performing preventive and corrective maintenance, as well as avionics modifications on airplanes and helicopters. I also had the opportunity to work in the quality control area, managing audits and developing procedure manuals. In the photos above, you can see me working on helicopters, Rotorcfrats are my favorites, a masterpiece of engineering.</p>
+      <Nav/>
+      
       {/* <ImageRow /> */}
     </>
   )
---
+++ SebastianMejia/src/components/Nav.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import './nav.css';
+import { AiOutlineHome } from 'react-icons/ai';
+import { GiHelicopter } from 'react-icons/gi';
+import { FaKeyboard } from 'react-icons/fa';
+
+// Define the Nav component
+export const Nav: React.FC = () => {
+  // State to track the active link
+  const [activeLink, setActiveLink] = useState<string>('#');
+
+  // Function to handle link click
+  const handleLinkClick = (href: string) => {
+    setActiveLink(href);
+  };
+
+  return (
+    <nav>
+      <a
+        href='#'
+        className={activeLink === '#' ? 'active' : ''}
+        onClick={() => handleLinkClick('#')}
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        href='#avionics'
+        className={activeLink === '#avionics' ? 'active' : ''}
+        onClick={() => handleLinkClick('#avionics')}
+      >
+        <GiHelicopter />
+      </a>
+      <a
+        href='#keyboard'
+        className={activeLink === '#keyboard' ? 'active' : ''}
+        onClick={() => handleLinkClick('#keyboard')}
+      >
+        <FaKeyboard />
+      </a>
+    </nav>
+  );
+};
+
+export default Nav;
+
