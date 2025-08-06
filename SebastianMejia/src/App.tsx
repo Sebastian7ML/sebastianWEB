3c52d198749d5e2dc76523549f99aeb5a5226b9a
Sebastian7ML <sebastian7ml@gmail.com>
2025-08-06 13:41:40 +0200

    added class main-content

--- SebastianMejia/src/App.tsx
+++ SebastianMejia/src/App.tsx
@@ -23,36 +23,38 @@ function App() {
     <>
       <HeroSection />
 
-      <div className='avionics-container'>
-        <AvionicsPics images={avionicsImages} />
+      <div className='main-content'>
+        <div className='avionics-container'>
+          <AvionicsPics images={avionicsImages} />
 
-        <div className='lateral-title-container'>
-          <h4 id='avionics'>Avionics Technician</h4>
+          <div className='lateral-title-container'>
+            <h4 id='avionics'>Avionics Technician</h4>
+          </div>
+
+          <div><p className='avionicsLife'>I worked for about 8 years at <a href="https://www.avionicacolombia.com.co/pages/avionics-installations" className='gulf-link' target='blank'>Gulf Coast Avionics Colombia</a>, performing preventive and corrective maintenance, as well as avionics modifications on airplanes and helicopters. I also had the opportunity to work in the quality control area, managing audits and developing procedure manuals. In the photos above, you can see me working on helicopters, Rotorcfrats are my favorites, a masterpiece of engineering.</p></div>
+
         </div>
 
-        <div><p className='avionicsLife'>I worked for about 8 years at <a href="https://www.avionicacolombia.com.co/pages/avionics-installations" className='gulf-link' target='blank'>Gulf Coast Avionics Colombia</a>, performing preventive and corrective maintenance, as well as avionics modifications on airplanes and helicopters. I also had the opportunity to work in the quality control area, managing audits and developing procedure manuals. In the photos above, you can see me working on helicopters, Rotorcfrats are my favorites, a masterpiece of engineering.</p></div>
+        <ImageInfoCard
+          title="Software Engineer"
+          description="While I was working on configuring and updating software in Garmin avionics suites, my curiosity began to grow and I decided to start studying engineering."
+          image={image1}
+          fromRight={false} // Set to true to position the first image on the right
+        />
 
+        <ImageInfoCard
+          title="Expatriate"
+          description="Just after my graduation, I embarked on a new adventure with my family — Spain was our destination. I currently work at Sabadell Airport, an airfield located north of Barcelona. I'm in charge of the technical office at C.F.A.C, S.L., where we perform maintenance on aircraft such as Cessna, Piper, and Robinson helicopters. We also inspect and repair Lycoming and Continental engines, as well as their components."
+          image={image2}
+          fromRight={true} // Set to false to position the second image on the left
+        />
+
+        <Nav />
+        <ContactForm formspreeEndpoint="https://formspree.io/f/mdkdwdza" />
+        
+        <WhatsAppButton />
       </div>
-
-      <ImageInfoCard
-        title="Software Engineer"
-        description="While I was working on configuring and updating software in Garmin avionics suites, my curiosity began to grow and I decided to start studying engineering."
-        image={image1}
-        fromRight={false} // Set to true to position the first image on the right
-      />
-
-      <ImageInfoCard
-        title="Expatriate"
-        description="Just after my graduation, I embarked on a new adventure with my family — Spain was our destination. I currently work at Sabadell Airport, an airfield located north of Barcelona. I'm in charge of the technical office at C.F.A.C, S.L., where we perform maintenance on aircraft such as Cessna, Piper, and Robinson helicopters. We also inspect and repair Lycoming and Continental engines, as well as their components."
-        image={image2}
-        fromRight={true} // Set to false to position the second image on the left
-      />
-      
-      <Nav />
-      <ContactForm formspreeEndpoint="https://formspree.io/f/mdkdwdza" />
       <Footer />
-      <WhatsAppButton />
-     
     </>
   )
 
